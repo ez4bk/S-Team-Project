81fee2aefd8ec87a42125847583f34747b6bc825
ez4bk <qq1034351717@gmail.com>
2022-10-04 15:49:05 -0400

    update venv

---
+++ config/server_info.py
@@ -0,0 +1,43 @@
+# coding=utf-8
+
+DEFAULT_BACKUP_SERVER_IP = "172.28.100.38"
+# 默认服务器密码
+DEFAULT_SERVER_PASSWORD_35_ = '35w_"{<L'
+DEFAULT_SERVER_PASSWORD_MjI = 'MjI1ZDY2NjQ'
+DEFAULT_SERVER_PASSWORD_NEW = 'ruijie-R@l&8g9.'
+
+RCOS_SSH_PASSWORD = "ruijie1688"
+RCOS_SSH_NAME = "ruijie1688"
+
+SERVER_SSH_PORT = 22  # ssh 端口
+NEW_SERVER_SSH_PORT = 9622  # ssh 端口
+NEW_SERVER_SSH_PWD = DEFAULT_SERVER_PASSWORD_NEW
+
+TERMINAL_SSH_PORT = 22  # ssh 端口
+NEW_TERMINAL_SSH_PORT = 10122  # ssh 端口
+SERVER_SSH_USER = "root"  # ssh登录用户名
+TERMINAL_NEW_SSH_USER = "rcd"  # ssh登录用户名
+TERMINAL_SSH_PASSWORD = "fjrjroot_123"  # 终端ssh密码
+NEW_TERMINAL_SSH_PASSWORD = "Rjrcd123@"  # 终端ssh密码
+NEW_ROOT_TERMINAL_SSH_PASSWORD = "rootRj369@"
+
+# server_ssh_password = '35w_"{<L'  # ssh登录密码
+server_ssh_password = DEFAULT_SERVER_PASSWORD_MjI  # ssh登录密码
+ssh_info_list = []
+sql_info_list = []
+
+
+DAYU = {'username': 'dayuuser', 'passwd': 'Thank-Y0U'}
+MILE = {'username': 'mileuser', 'passwd': 'Thank-Y0U'}
+TANG = {'username': 'tanguser', 'passwd': 'rcos_2020$Rj'}
+WUKONG = {'username': 'wukonguser', 'passwd': 'rcos_2020$Rj'}
+RCDC = {'username': 'rcdcuser', 'passwd': 'rcos_2020$Rj'}
+SUNNY = {'username': 'oc_admin', 'passwd': '^sunny256$'}
+# 个人数据库配置
+DAYU_DB = {'username': DAYU['username'], 'passwd': DAYU['passwd'], 'db_name': 'dayudb'}
+MELI_DB = {'username': MILE['username'], 'passwd': MILE['passwd'], 'db_name': 'miledb'}
+TANG_DEFAULT_DB = {'username': TANG['username'], 'passwd': TANG['passwd'], 'db_name': 'tangdefaultdb'}
+TANG_PUBLIC_DB = {'username': TANG['username'], 'passwd': TANG['passwd'], 'db_name': 'tangpublicdb'}
+RCDC_DEFAULT_DB = {'username': RCDC['username'], 'passwd': RCDC['passwd'], 'db_name': 'rcdc_default'}
+RCDC_PUBLIC_DB = {'username': RCDC['username'], 'passwd': RCDC['passwd'], 'db_name': 'rcdc_public'}
+RCDC_RCO_DEFAULT_DB = {'username': RCDC['username'], 'passwd': RCDC['passwd'], 'db_name': 'rcdc_rco_default'}
