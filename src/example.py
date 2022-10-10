1a777547a7a7c10e84ef5f63445813a05506116b
ez4bk <qq1034351717@gmail.com>
2022-10-09 20:58:55 -0400

    add some example ui files

---
+++ src/example.py
@@ -0,0 +1,21 @@
+import sys
+
+from PyQt6 import QtWidgets  # import PyQt6 widgets
+
+# Create the application object
+app = QtWidgets.QApplication(sys.argv)
+
+# Create the form object
+first_window = QtWidgets.QWidget()
+
+# Set window size
+first_window.resize(400, 300)
+
+# Set the form title
+first_window.setWindowTitle("The first pyqt program")
+
+# Show form
+first_window.show()
+
+# Run the program
+sys.exit(app.exec())
