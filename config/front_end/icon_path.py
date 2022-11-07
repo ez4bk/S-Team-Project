05516c5c647efa9123a50bb85ab0acdc18448892
ez4bk <qq1034351717@gmail.com>
2022-11-07 17:21:15 -0500

    update

--- config/front_end/icon_path.py
+++ config/front_end/icon_path.py
@@ -5,3 +5,11 @@ settings_icon = "src/resource/img/buttom/设置配置_setting-config.svg"
 exit_icon = "src/resource/img/buttom/退出_logout.svg"
 list_widget_icons = [game_icon, library_icon, settings_icon, exit_icon]
 switch_child_icon = "src/resource/img/buttom/切换_switch.svg"
+
+# FamiOwl Signin
+title_img = "src/resource/famiowl_title.png"
+owl_gif = "src/resource/owl_gif.gif"
+signin_icon = "src/resource/login_icon.png"
+
+# FamiOwl Signup
+child_img = "src/resource/signup_child.png"
