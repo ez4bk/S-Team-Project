79c4d983d5349376bef6b85acfe9cb4c9e640256
Wen Huang <wendi.huang0125@gmail.com>
2022-11-11 14:31:41 -0500

    change the name of table game_library to game_store

--- config/sql_query/client_query.py
+++ config/sql_query/client_query.py
@@ -1,5 +1,5 @@
 # show game in the distributing platform where id = input id
-show_game = 'select * from game_library where game_id = "{0}";'
+show_game = 'select * from game_store where game_id = "{0}";'
 
 # show top x games
-show_top_game = 'select * from game_library order by copies_sold desc limit "{0}";'
+show_top_game = 'select * from game_store order by copies_sold desc limit "{0}";'
