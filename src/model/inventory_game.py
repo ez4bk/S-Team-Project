289471940c511bf8a790d7c592652081a6b0dc9c
Wen Huang <wendi.huang0125@gmail.com>
2022-11-18 16:31:54 -0500

    add accumulate time

--- src/model/inventory_game.py
+++ src/model/inventory_game.py
@@ -3,6 +3,11 @@ import os
 from config.project_info import DOWNLOAD_DIR
 from src.model.game import Game
 
+from lib.base_lib.sql.sql_utils import SqlUtils
+from config.sql_query.game_query import time_record_update,time_record_check
+
+sql_utils = SqlUtils()
+
 
 class InventoryGame(Game):
     def __init__(self, game, fami_parent, local_path=''):
@@ -25,5 +30,10 @@ class InventoryGame(Game):
     def delete(self):
         return 0
 
+# Wendi: get kid id from parents table, use kid id to find
+    def accumulate_playtime(self):
+#       kid_id = sql_utils.sql_exec
+#        record_exist = sql_utils.sql_exec(time_record_check.format(kid_id, self.return_game_id()),1)[1][1]
+        return 0
     def __str__(self):
         return ""
