e91b8d33f905da72bad138cf559feb08acef6b91
Wen Huang <wendi.huang0125@gmail.com>
2022-11-18 16:19:45 -0500

    add rating

--- config/sql_query/game_query.py
+++ config/sql_query/game_query.py
@@ -4,3 +4,9 @@ get_download_dir_path = 'select download_location from game_store where game_id 
 
 # get like and dislike
 get_ratings = 'select like_count, dislike_count from game_store where game_id = "{0}";'
+
+# check if time record already exist, if 0 DNE if 1 exists
+time_record_check = 'select exists(select * from kid_playtime where kid_id = "{0}" and game_id = "{1}") as result;'
+
+# insert new time
+time_record_update = 'update kid_playtime set playtime = playtime + "{0}" where kid_id = "{1}" and game_id ="{2}";'
