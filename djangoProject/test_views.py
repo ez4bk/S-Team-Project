bd23cf74fdd5452af0b05c282c5a85b47fc348e1
XYM-2001 <xmeng975@gmail.com>
2022-12-02 15:56:07 -0500

    Implemented some test cases

--- djangoProject/test_views.py
+++ djangoProject/test_views.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from django.test import Client
+from django.test import Client, RequestFactory
 from djangoProject.views import *
 from django.shortcuts import render, HttpResponse, redirect
 from djangoProject.models import *
@@ -8,8 +8,25 @@ from django.contrib import messages
 
 aes_pass = AESCipher()
 
-# class models_tests(TestCase):
-#     def setUp(self):
-#         c = Client()
-#         response = c.post('/login/', {'user_id': 'john', 'password': 'smith'})
-#         response.status_code
+
+class views_tests(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create(user_name='test_account', password=aes_pass.encrypt_main('test_pw'), user_id='123')
+        Children.objects.create(kids_name='Mike', time_limit=100, parent_id='123', icon_image='3')
+
+    def test_login_page(self):
+        request = self.factory.get('login/')
+        request.user = self.user
+        response = sign(request)
+        self.assertEqual(response.status_code, 200)
+
+    # def test_login_post(self):
+    #     # request = self.factory.post('login/', {'user_id': 123, 'password': 'test_pw'})
+    #     # request.user = self.user
+    #     # request.session['user_id'] = ''
+    #     # response = sign(request)
+    #     c = Client()
+    #     # response = c.post('login/', {'user_id': 123, 'password': 'test_pw'})
+    #     response = self.client.post('login/', {'user_id': 123, 'password': 'test_pw'})
+    #     print(response.status_code)
