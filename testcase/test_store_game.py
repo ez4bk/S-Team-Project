a342c40e476fcc8f41a6da88c553da50876213dd
Wen Huang <wendi.huang0125@gmail.com>
2022-12-09 12:59:44 -0500

    test inventory game, store game

--- testcase/test_store_game.py
+++ testcase/test_store_game.py
@@ -15,3 +15,41 @@ class TestStoreGame(object):
             store_game.add_to_inventory(fami_parent, DOWNLOAD_DIR)
         except:
             assert False, "add to inventory failed"
+
+    def test_download(self):
+        fami_parent = FamiParent()
+        store_game = StoreGame(6, "Snake", "/home/famiowl_files/game_icons/icon_gameid=6.png",
+                               "/home/famiowl_files/snake.py", "Eat and grow your snake!", 8)
+        fami_parent.get_parent_info_query('test5@test.com', 'test')
+        try:
+            store_game.download()
+        except:
+            assert False, "download failed"
+
+    def test_add_like(self):
+        fami_parent = FamiParent()
+        store_game = StoreGame(6, "Snake", "/home/famiowl_files/game_icons/icon_gameid=6.png",
+                               "/home/famiowl_files/snake.py", "Eat and grow your snake!", 8)
+        fami_parent.get_parent_info_query('test5@test.com', 'test')
+        correct_res = store_game.return_like_count()
+        correct_res += 1
+        try:
+            store_game.add_like()
+        except:
+            assert False, "add like failed"
+        res = store_game.return_like_count()
+        assert res == correct_res, "add like failed, value incorrect"
+
+    def test_remove_like(self):
+        fami_parent = FamiParent()
+        store_game = StoreGame(6, "Snake", "/home/famiowl_files/game_icons/icon_gameid=6.png",
+                               "/home/famiowl_files/snake.py", "Eat and grow your snake!", 8)
+        fami_parent.get_parent_info_query('test5@test.com', 'test')
+        correct_res = store_game.return_like_count()
+        correct_res -= 1
+        try:
+            store_game.remove_like()
+        except:
+            assert False, "remove like failed"
+        res = store_game.return_like_count()
+        assert res == correct_res, "remove like failed, value incorrect"
