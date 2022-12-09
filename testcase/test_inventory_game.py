a1376e105e98dd7396ece0e098479f61ada137a2
Wen Huang <wendi.huang0125@gmail.com>
2022-12-09 12:10:35 -0500

    test inventory game

--- testcase/test_inventory_game.py
+++ testcase/test_inventory_game.py
@@ -1,4 +1,8 @@
 from src.model.inventory_game import InventoryGame
+from src.model.store_game import StoreGame
+from src.model.fami_parent import FamiParent
+
+
 class TestInventoryGame(object):
 
     def test_run_game(self):
@@ -8,13 +12,39 @@ class TestInventoryGame(object):
         return
 
     def test_get_like_count(self):
-        return
+        store_game = StoreGame(6, "Snake", "/home/famiowl_files/game_icons/icon_gameid=6.png",
+                               "/home/famiowl_files/snake.py", "Eat and grow your snake!", 8)
+        fami_parent = FamiParent()
+        inventory_game = InventoryGame(store_game, fami_parent)
+        assert inventory_game.return_like_count() != 8, "get like count failed"
 
     def test_hit_like(self):
-        return
+        store_game = StoreGame(6, "Snake", "/home/famiowl_files/game_icons/icon_gameid=6.png",
+                               "/home/famiowl_files/snake.py", "Eat and grow your snake!", 8)
+        fami_parent = FamiParent()
+        inventory_game = InventoryGame(store_game, fami_parent)
+        try:
+            inventory_game.hit_like()
+        except:
+            assert False, 'hit like failed'
 
     def test_hit_unlike(self):
-        return
+        store_game = StoreGame(6, "Snake", "/home/famiowl_files/game_icons/icon_gameid=6.png",
+                               "/home/famiowl_files/snake.py", "Eat and grow your snake!", 8)
+        fami_parent = FamiParent()
+        inventory_game = InventoryGame(store_game, fami_parent)
+        try:
+            inventory_game.hit_unlike()
+        except:
+            assert False, 'hit unlike failed'
 
     def test_sync_like(self):
+        store_game = StoreGame(6, "Snake", "/home/famiowl_files/game_icons/icon_gameid=6.png",
+                               "/home/famiowl_files/snake.py", "Eat and grow your snake!", 8)
+        fami_parent = FamiParent()
+        inventory_game = InventoryGame(store_game, fami_parent)
+        try:
+            inventory_game.sync_likes()
+        except:
+            assert False, 'sync like failed'
         return
