9708c70c7202c24f76f67cc0868046140a3c98fd
Wen Huang <wendi.huang0125@gmail.com>
2022-12-09 11:37:19 -0500

    test inventory game

---
+++ testcase/test_inventory_game.py
@@ -0,0 +1,20 @@
+from src.model.inventory_game import InventoryGame
+class TestInventoryGame(object):
+
+    def test_run_game(self):
+        return
+
+    def test_stop_game(self):
+        return
+
+    def test_get_like_count(self):
+        return
+
+    def test_hit_like(self):
+        return
+
+    def test_hit_unlike(self):
+        return
+
+    def test_sync_like(self):
+        return
