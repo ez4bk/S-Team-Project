07c456dc7964317321705bcd6ff7818ae6b1d86b
ez4bk <qq1034351717@gmail.com>
2022-12-13 18:06:16 -0500

    update uml

--- main.py
+++ main.py
@@ -5,16 +5,11 @@ from PyQt6 import QtWidgets
 from lib.base_lib.sql.sql_utils import SqlUtils
 from src.control.signin_control import SigninWindow
 
-# from src.control.famiowl_client_control import FamiOwlClientWindow
-# from src.control.famiowl_child_new_control import FamiOwlChildNew
-
 sql_utils = SqlUtils()
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     signin_window = SigninWindow()
     signin_window.show()
-    # w = FamiOwlChildNew()
-    # w.show()
 
     sys.exit(app.exec())
